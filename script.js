950c6fa4c4b820c1d25a473ac8afcf9aaa54f213
maheshhattimare <maheshhattimare02@gmail.com>
2024-06-14 15:28:18 +0530

    Implemented checkbox functionality

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const passwordBox=document.getElementById("password");
 const generateBtn=document.getElementById("generateBtn");
 const copyBtn=document.getElementById("copy-btn");
+const crossBtn=document.getElementById("cross-icon");
+const messageBox=document.getElementById("message");
+
+const checkBox1=document.getElementById("check-box1");
+const checkBox2=document.getElementById("check-box2");
+const checkBox3=document.getElementById("check-box3");
+const checkBox4=document.getElementById("check-box4");
+
 
 const maxLength=12;
 
@@ -9,11 +17,25 @@ const LowerCase="abcdefghijklmnopqrstuvwxyz";
 const number="0123456789";
 const symbol="!@#$%&*()-=_+[]{}|;:/<>?"
 
-const allChars=upperCase+LowerCase+number+symbol;
-
 function createPassword(){
     let password="";
+    let allChars=""
 
+    //check box
+    if(!(checkBox1.checked) && !(checkBox2.checked) && !(checkBox3.checked) && !(checkBox4.checked)){
+        messageBox.style.display="block";
+        return;
+    
+    }
+    else{
+        messageBox.style.display="none";
+        
+        allChars+=checkBox1.checked? upperCase: "";
+        allChars+=checkBox2.checked? LowerCase: "";
+        allChars+=checkBox3.checked? number: "";
+        allChars+=checkBox4.checked? symbol: "";
+    }
+    
     // password+=upperCase[Math.floor(Math.random()*upperCase.length)];
     // password+=LowerCase[Math.floor(Math.random()*LowerCase.length)];
     // password+=number[Math.floor(Math.random()*number.length)];
@@ -25,21 +47,24 @@ function createPassword(){
     passwordBox.value=password;
 }
 
+
 generateBtn.addEventListener('click',()=>{
-    createPassword();
+  
+        createPassword();
+
+    
+    
 });
 
 //copy button function
 copyBtn.addEventListener('click',()=>{
     passwordBox.select();
     document.execCommand('copy');
-
-    
 });
 
-passwordBox.addEventListener('keydown',function(e){
-    
+//delete button
+crossBtn.addEventListener('click',()=>{
+    passwordBox.value=""
 })
-
 
 
