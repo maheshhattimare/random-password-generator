47443b7dd0f1fee0b11d0ef52a71a81fc5c082cc
maheshhattimare <maheshhattimare02@gmail.com>
2024-06-16 19:57:30 +0530

    Update UI and add custom password length feature

--- script.js
+++ script.js
@@ -1,70 +1,71 @@
-const passwordBox=document.getElementById("password");
-const generateBtn=document.getElementById("generateBtn");
-const copyBtn=document.getElementById("copy-btn");
-const crossBtn=document.getElementById("cross-icon");
-const messageBox=document.getElementById("message");
+var slider=document.getElementById('length-slider');
+const lengthValue=document.getElementById('length-value');
+const generateBtn=document.getElementById('generate-btn');
+const inputBox=document.getElementById('input-box');
+const copyMessage = document.getElementById('copy-message');
+const warningMessage = document.getElementById('warning-message');
 
-const checkBox1=document.getElementById("check-box1");
-const checkBox2=document.getElementById("check-box2");
-const checkBox3=document.getElementById("check-box3");
-const checkBox4=document.getElementById("check-box4");
-
-
-const maxLength=12;
+const uppercaseBox=document.getElementById("uppercase");
+const lowercaseBox=document.getElementById("lowercase");
+const numberBox=document.getElementById("number");
+const symbolBox=document.getElementById("symbol");
 
 const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LowerCase="abcdefghijklmnopqrstuvwxyz";
 const number="0123456789";
 const symbol="!@#$%&*()-=_+[]{}|;:/<>?"
 
-function createPassword(){
-    let password="";
-    let allChars=""
-
-    //check box
-    if(!(checkBox1.checked) && !(checkBox2.checked) && !(checkBox3.checked) && !(checkBox4.checked)){
-        messageBox.style.display="block";
+//slider
+slider.addEventListener('input',function(){
+    lengthValue.textContent=this.value;
+});
+//button code
+generateBtn.addEventListener('click',()=>{
+    if(!(uppercaseBox.checked) && !(lowercaseBox.checked) && !(numberBox.checked) && !(symbolBox.checked)){
+        warningMessage.style.display="block"
+        setTimeout(() => {
+            warningMessage.style.display="none"
+        }, 3000);
         return;
     
     }
     else{
-        messageBox.style.display="none";
-        
-        allChars+=checkBox1.checked? upperCase: "";
-        allChars+=checkBox2.checked? LowerCase: "";
-        allChars+=checkBox3.checked? number: "";
-        allChars+=checkBox4.checked? symbol: "";
-    }
-    
-    // password+=upperCase[Math.floor(Math.random()*upperCase.length)];
-    // password+=LowerCase[Math.floor(Math.random()*LowerCase.length)];
-    // password+=number[Math.floor(Math.random()*number.length)];
-    // password+=symbol[Math.floor(Math.random()*symbol.length)];
-    
-    while(12 > password.length){
-        password+=allChars[Math.floor(Math.random()*allChars.length)];
-    }
-    passwordBox.value=password;
-}
-
-
-generateBtn.addEventListener('click',()=>{
-  
         createPassword();
-
-    
-    
-});
-
-//copy button function
-copyBtn.addEventListener('click',()=>{
-    passwordBox.select();
-    document.execCommand('copy');
-});
-
-//delete button
-crossBtn.addEventListener('click',()=>{
-    passwordBox.value=""
+    }
 })
 
+function createPassword(){
+    let passLength=slider.value;
+    console.log(passLength);
+    let password="";
+    let allChars="";
 
+    allChars+=uppercaseBox.checked? upperCase: "";
+    allChars+=lowercaseBox.checked? LowerCase: "";
+    allChars+=numberBox.checked? number: "";
+    allChars+=symbolBox.checked? symbol: "";
+
+    while(passLength>password.length){
+        password+=allChars[Math.floor(Math.random()*allChars.length)];
+        
+    }
+    inputBox.value=password;
+}
+
+//copy password code
+inputBox.addEventListener('click',()=>{
+    inputBox.select();
+    document.execCommand('copy');
+    showCopyMessage();
+})
+
+//if all checkbox false then:
+function showCopyMessage(){
+    if(inputBox.value!==""){
+        copyMessage.classList.add('show');
+        setTimeout(() => {
+            copyMessage.classList.remove('show');
+            
+        }, 2000);
+    }
+}
